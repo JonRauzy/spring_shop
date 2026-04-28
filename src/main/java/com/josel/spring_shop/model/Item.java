dd10d9aa5ffb5827f543045f28ce4dca52440762
JonRauzy <jonathan.rauzy@gmail.com>
2026-04-28 14:39:49 +0200

    item model + dto's

--- src/main/java/com/josel/spring_shop/model/Item.java
+++ src/main/java/com/josel/spring_shop/model/Item.java
@@ -1,6 +1,28 @@
 package com.josel.spring_shop.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int quantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Order> orders = new ArrayList<>();
 }
--- src/main/java/com/josel/spring_shop/model/Order.java
+++ src/main/java/com/josel/spring_shop/model/Order.java
@@ -1,6 +1,4 @@
 package com.josel.spring_shop.model;
 
-import jakarta.persistence.Entity;
-
 public class Order {
 }
