15decbeb59d56b12a3b75f71910f071a5bcc62e2
BaselAdoum <adoumbasel@gmail.com>
2026-04-28 14:41:44 +0200

    order package  added

--- src/main/java/com/josel/spring_shop/model/Order.java
+++ src/main/java/com/josel/spring_shop/model/Order.java
@@ -1,6 +1,31 @@
 package com.josel.spring_shop.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "items")
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinTable (name = "order_item",
+    JoinColumns = @JoinColumn (name = "item_id"),
+    inverseJoinColums = @JoinColumns (name = "order_id"))
+    private List<Item> items = new ArrayList<>();
+
+
+
 }
